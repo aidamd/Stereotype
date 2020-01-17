d89cf7fe8da3684b6156ef765cfbd966e4193638
aiida- <aida.m91@gmail.com>
2020-01-17 09:41:43 -0800

    convert_to_tensor

--- model.py
+++ model.py
@@ -13,10 +13,10 @@ class Multi(RNN):
         RNN.build(self, data)
         self.vars["annotators"] = tf.placeholder(tf.int32, shape=[None],
                                                  name="Annotators")
-        self.vars["loss"] = tf.Tensor([self.vars["loss-" + name] for name in
-                                         list(data.target_names.keys())])
-        self.vars["accuracy"] = tf.Tensor([self.vars["accuracy-" + name] for name in
-                                         list(data.target_names.keys())])
+        self.vars["loss"] = tf.convert_to_tensor([self.vars["loss-" + name] for name in
+                                         list(data.target_names.keys())], tf.float32)
+        self.vars["accuracy"] = tf.convert_to_tensor([self.vars["accuracy-" + name] for name in
+                                         list(data.target_names.keys())], tf.float32, )
         self.vars["joint_loss"] = tf.gather(self.vars["loss"], self.vars["annotators"])
 
         #self.vars["joint_loss"], self.vars["joint_accuracy"] = \
